87555f5fd62d44ee2ffb66240d273398f3c31019
pure-js <evolution.is.already.underway@gmail.com>
2017-11-23 20:27:37 +0300

    add test case for each popular web browser

--- spec/browser-deprecated.spec.js
+++ spec/browser-deprecated.spec.js
@@ -1,64 +1,50 @@
-describe("Player", function() {
-  var Player = require('../lib/jasmine_examples/Player');
-  var Song = require('../lib/jasmine_examples/Song');
+describe('Browser detection', () => {
   const detectBrowser = require('../src/browser-deprecated');
-  var player;
-  var song;
 
-  beforeEach(function() {
-    player = new Player();
-    song = new Song();
-  });
+  it('should be able to detect a browser', () => {
+    const ChromeUserAgent = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) 
+      AppleWebKit/537.36 (KHTML, like Gecko) 
+      Chrome/62.0.3202.94 Safari/537.36`;
 
-  it('should be able to detect a browser', function() {
-    const chrome = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' +
-      'AppleWebKit/537.36 (KHTML, like Gecko) ' +
-      'Chrome/62.0.3202.94 Safari/537.36';
+    const FirefoxUserAgent = `Mozilla/5.0 
+      (Windows NT 10.0; Win64; x64; rv:57.0) 
+      Gecko/20100101 Firefox/57.0`;
 
-    expect(detectBrowser(chrome)).toEqual({
+    const EdgeUserAgent = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) 
+      AppleWebKit/537.36 (KHTML, like Gecko) 
+      Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063`;
+
+    const OperaUserAgent = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) 
+      AppleWebKit/537.36 (KHTML, like Gecko) 
+      Chrome/62.0.3202.89 Safari/537.36 OPR/49.0.2725.39`;
+
+    const IEUserAgent = `Mozilla/5.0 (Windows NT 10.0; WOW64; 
+      Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; 
+      .NET CLR 3.0.30729; .NET CLR 3.5.30729; rv:11.0) like Gecko`;
+
+    expect(detectBrowser(ChromeUserAgent)).toEqual({
       name: 'Chrome',
       version: 62,
     });
-  });
 
-  describe("when song has been paused", function() {
-    beforeEach(function() {
-      player.play(song);
-      player.pause();
+    expect(detectBrowser(FirefoxUserAgent)).toEqual({
+      name: 'Firefox',
+      version: 57,
     });
 
-    it("should indicate that the song is currently paused", function() {
-      expect(player.isPlaying).toBeFalsy();
-
-      // demonstrates use of 'not' with a custom matcher
-      expect(player).not.toBePlaying(song);
+    expect(detectBrowser(EdgeUserAgent)).toEqual({
+      name: 'Edge',
+      version: 57,
     });
 
-    it("should be possible to resume", function() {
-      player.resume();
-      expect(player.isPlaying).toBeTruthy();
-      expect(player.currentlyPlayingSong).toEqual(song);
+    expect(detectBrowser(OperaUserAgent)).toEqual({
+      name: 'Opera',
+      version: 49,
     });
-  });
 
-  // demonstrates use of spies to intercept and test method calls
-  it("tells the current song if the user has made it a favorite", function() {
-    spyOn(song, 'persistFavoriteStatus');
-
-    player.play(song);
-    player.makeFavorite();
-
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  });
-
-  //demonstrates use of expected exceptions
-  describe("#resume", function() {
-    it("should throw an exception if song is already playing", function() {
-      player.play(song);
-
-      expect(function() {
-        player.resume();
-      }).toThrowError("song is already playing");
+    expect(detectBrowser(IEUserAgent)).toEqual({
+      name: 'IE',
+      version: 11,
     });
   });
 });
