8e2a93422ba37a241f6c209e7cb0ceb568649d89
pure-js <evolution.is.already.underway@gmail.com>
2017-11-23 18:07:14 +0300

    add EDGE to regular expression

--- src/browser-deprecated.js
+++ src/browser-deprecated.js
@@ -1,3 +1,29 @@
+// Detecting browser version
+function detectBrowser() {
+  let ua = navigator.userAgent;
+  let tem;
+  let M = ua.match(/(opera|chrome|safari|firefox|msie|edge|trident(?=\/))\/?\s*(\d+)/i) || [];
+
+  if (/trident/i.test(M[1])) {
+    tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+    return 'IE ' + (tem[1] || '');
+  }
+
+  if (M[1] === 'Chrome') {
+    tem = ua.match(/\bOPR\/(\d+)/);
+    if (tem !== null) return 'Opera ' + tem[1];
+  }
+
+  M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+
+  if ((tem = ua.match(/version\/(\d+)/i)) !== null) M.splice(1, 1, tem[1]);
+
+  return {
+    name: M[0],
+    version: M[1],
+  };
+}
+
 function browserIsDeprecated(browsers) {
   const defaults = {
     browsers: {
@@ -8,33 +34,6 @@ function browserIsDeprecated(browsers) {
     },
   };
 
-  // Detecting browser version
-  function detectBrowser() {
-    let ua = navigator.userAgent;
-    let tem;
-    let M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-
-    if (/trident/i.test(M[1])) {
-      tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
-      return 'IE ' + (tem[1] || '');
-    }
-
-    if (M[1] === 'Chrome') {
-      tem = ua.match(/\bOPR\/(\d+)/);
-      if (tem !== null) return 'Opera ' + tem[1];
-    }
-
-    M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
-
-    if ((tem = ua.match(/version\/(\d+)/i)) !== null) M.splice(1, 1, tem[1]);
-
-    let browser = {
-      name: M[0],
-      version: M[1],
-    };
-
-    return browser;
-  }
 
 
   function browserDeprecated(
